be73972bb0a08f7e6213393d4ef272dff48a395c
geoffrey <geoffreymwangi5378@gmail.com>
2017-12-15 09:53:25 +0300

    db uploads

--- dbs/geoff/pro_kazi (2).sql
+++ dbs/geoff/pro_kazi (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2017 at 02:24 PM
+-- Generation Time: Dec 15, 2017 at 07:50 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_cases`
 --
 
+DROP TABLE IF EXISTS `tbl_cases`;
 CREATE TABLE `tbl_cases` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -53,6 +54,7 @@ INSERT INTO `tbl_cases` (`id`, `name`, `status`, `deleted`, `case_procedure`, `d
 -- Table structure for table `tbl_case_procedures`
 --
 
+DROP TABLE IF EXISTS `tbl_case_procedures`;
 CREATE TABLE `tbl_case_procedures` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -73,6 +75,7 @@ INSERT INTO `tbl_case_procedures` (`id`, `name`, `deleted`) VALUES
 -- Table structure for table `tbl_case_status`
 --
 
+DROP TABLE IF EXISTS `tbl_case_status`;
 CREATE TABLE `tbl_case_status` (
   `id` int(50) NOT NULL,
   `status` varchar(100) NOT NULL
@@ -92,6 +95,7 @@ INSERT INTO `tbl_case_status` (`id`, `status`) VALUES
 -- Table structure for table `tbl_case_type`
 --
 
+DROP TABLE IF EXISTS `tbl_case_type`;
 CREATE TABLE `tbl_case_type` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -109,9 +113,158 @@ INSERT INTO `tbl_case_type` (`id`, `name`, `deleted`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_checklists`
+--
+
+DROP TABLE IF EXISTS `tbl_checklists`;
+CREATE TABLE `tbl_checklists` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `deleted` int(11) NOT NULL,
+  `performed_on` int(11) NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_checklists`
+--
+
+INSERT INTO `tbl_checklists` (`id`, `name`, `deleted`, `performed_on`, `comment`) VALUES
+(7, 'sddssd', 1, 1, 'sddssd'),
+(8, 'Check ICT router', 0, 1, 'The router should be good'),
+(9, 'new order here', 0, 2, 'ddfdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_checklist_performed_on`
+--
+
+DROP TABLE IF EXISTS `tbl_checklist_performed_on`;
+CREATE TABLE `tbl_checklist_performed_on` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `deleted` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_checklist_performed_on`
+--
+
+INSERT INTO `tbl_checklist_performed_on` (`id`, `name`, `deleted`) VALUES
+(1, 'ICT', 0),
+(2, 'Maintenance', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_checklist_status`
+--
+
+DROP TABLE IF EXISTS `tbl_checklist_status`;
+CREATE TABLE `tbl_checklist_status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `deleted` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_checklist_status`
+--
+
+INSERT INTO `tbl_checklist_status` (`id`, `name`, `deleted`) VALUES
+(1, 'Submitted', 0),
+(2, 'Escalated', 0),
+(3, 'Closed', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_checklist_tasks`
+--
+
+DROP TABLE IF EXISTS `tbl_checklist_tasks`;
+CREATE TABLE `tbl_checklist_tasks` (
+  `id` int(11) NOT NULL,
+  `ref_no` text NOT NULL,
+  `status` int(11) NOT NULL,
+  `performed_by` int(11) NOT NULL,
+  `performed_on` date NOT NULL,
+  `escalate_to` int(11) NOT NULL,
+  `deleted` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_checklist_tasks`
+--
+
+INSERT INTO `tbl_checklist_tasks` (`id`, `ref_no`, `status`, `performed_by`, `performed_on`, `escalate_to`, `deleted`) VALUES
+(7, '1513304821', 2, 5, '2017-12-15', 114, 0),
+(8, '1513316522', 2, 5, '2017-12-15', 113, 0),
+(9, '1513318031', 1, 5, '2017-12-15', 0, 0),
+(10, '1513318081', 2, 5, '2017-12-15', 114, 0),
+(11, '1513318093', 1, 5, '2017-12-15', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_checklist_task_checks`
+--
+
+DROP TABLE IF EXISTS `tbl_checklist_task_checks`;
+CREATE TABLE `tbl_checklist_task_checks` (
+  `id` int(11) NOT NULL,
+  `check_item` int(11) NOT NULL,
+  `status` int(11) NOT NULL COMMENT '1 passed 0 failed',
+  `comment` text,
+  `checklist_task` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_checklist_task_checks`
+--
+
+INSERT INTO `tbl_checklist_task_checks` (`id`, `check_item`, `status`, `comment`, `checklist_task`) VALUES
+(7, 8, 1, NULL, 7),
+(8, 9, 0, 'ksdkmsdk', 7),
+(9, 8, 1, '', 8),
+(10, 9, 0, '', 8),
+(11, 8, 1, '', 9),
+(12, 9, 1, '', 9),
+(13, 8, 1, '', 10),
+(14, 9, 0, '', 10),
+(15, 8, 1, '', 11),
+(16, 9, 1, '', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_checklist_types`
+--
+
+DROP TABLE IF EXISTS `tbl_checklist_types`;
+CREATE TABLE `tbl_checklist_types` (
+  `id` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `deleted` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_checklist_types`
+--
+
+INSERT INTO `tbl_checklist_types` (`id`, `type`, `deleted`) VALUES
+(1, 'Daily', 0),
+(2, 'Weekly', 1),
+(3, 'Monthly', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_cust_supplier_checks`
 --
 
+DROP TABLE IF EXISTS `tbl_cust_supplier_checks`;
 CREATE TABLE `tbl_cust_supplier_checks` (
   `id` int(11) NOT NULL,
   `type` int(11) NOT NULL COMMENT '1 customer, 2 supplier',
@@ -127,7 +280,9 @@ CREATE TABLE `tbl_cust_supplier_checks` (
 --
 
 INSERT INTO `tbl_cust_supplier_checks` (`id`, `type`, `cust_supp_id`, `checked_on`, `status`, `checked_by`, `deleted`) VALUES
-(4, 1, 1, 2017, 1, 5, 0);
+(33, 2, 1, 2017, 0, 5, 0),
+(34, 1, 16, 2017, 0, 5, 0),
+(35, 1, 17, 2017, 1, 5, 0);
 
 -- --------------------------------------------------------
 
@@ -135,6 +290,7 @@ INSERT INTO `tbl_cust_supplier_checks` (`id`, `type`, `cust_supp_id`, `checked_o
 -- Table structure for table `tbl_cust_supplier_specific_checks`
 --
 
+DROP TABLE IF EXISTS `tbl_cust_supplier_specific_checks`;
 CREATE TABLE `tbl_cust_supplier_specific_checks` (
   `id` int(11) NOT NULL,
   `check_item` int(11) NOT NULL,
@@ -150,10 +306,13 @@ CREATE TABLE `tbl_cust_supplier_specific_checks` (
 --
 
 INSERT INTO `tbl_cust_supplier_specific_checks` (`id`, `check_item`, `status`, `comment`, `expiry_date`, `customer_id`, `deleted`) VALUES
-(23, 12, 0, NULL, NULL, 4, 0),
-(24, 13, 0, 'test comment new', '2017-12-12T13:18:00.000Z', 4, 0),
-(25, 15, 0, NULL, NULL, 4, 0),
-(26, 17, 0, NULL, NULL, 4, 0);
+(382, 24, 0, '', '', 32, 0),
+(404, 21, 1, '', '', 35, 0),
+(405, 22, 1, '', '', 35, 0),
+(407, 21, 0, '', '', 33, 0),
+(408, 22, 0, NULL, '', 33, 0),
+(409, 21, 0, 'New comment here', '2017-12-04', 34, 0),
+(410, 22, 0, '', '2017-12-11', 34, 0);
 
 -- --------------------------------------------------------
 
@@ -161,6 +320,7 @@ INSERT INTO `tbl_cust_supplier_specific_checks` (`id`, `check_item`, `status`, `
 -- Table structure for table `tbl_cust_supp_checkitems`
 --
 
+DROP TABLE IF EXISTS `tbl_cust_supp_checkitems`;
 CREATE TABLE `tbl_cust_supp_checkitems` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -173,13 +333,11 @@ CREATE TABLE `tbl_cust_supp_checkitems` (
 --
 
 INSERT INTO `tbl_cust_supp_checkitems` (`id`, `name`, `deleted`, `performed_on`) VALUES
-(12, 'DSKSDM', 1, '1,2'),
-(13, 'Neema Licence', 1, '1,2'),
-(14, 'kmsdksd', 1, ''),
-(15, 'Government certificates', 1, '1,2'),
-(16, 'test first certificate', 1, '2'),
-(17, 'Legal laws', 0, '1,2'),
-(18, 'Kra pin', 0, '2');
+(21, 'Kra Pin', 0, '1,2'),
+(22, 'Account opening form', 0, '1,2'),
+(23, 'New item', 1, '1,'),
+(24, 'National Identity', 1, '1,2'),
+(25, 'First item', 1, '');
 
 -- --------------------------------------------------------
 
@@ -187,6 +345,7 @@ INSERT INTO `tbl_cust_supp_checkitems` (`id`, `name`, `deleted`, `performed_on`)
 -- Table structure for table `tbl_cust_supp_status`
 --
 
+DROP TABLE IF EXISTS `tbl_cust_supp_status`;
 CREATE TABLE `tbl_cust_supp_status` (
   `id` int(11) NOT NULL,
   `status` varchar(200) NOT NULL
@@ -206,6 +365,7 @@ INSERT INTO `tbl_cust_supp_status` (`id`, `status`) VALUES
 -- Table structure for table `tbl_doc_escalation_n_reminders`
 --
 
+DROP TABLE IF EXISTS `tbl_doc_escalation_n_reminders`;
 CREATE TABLE `tbl_doc_escalation_n_reminders` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(200) NOT NULL,
@@ -222,7 +382,8 @@ CREATE TABLE `tbl_doc_escalation_n_reminders` (
 --
 
 INSERT INTO `tbl_doc_escalation_n_reminders` (`id`, `name`, `doc_type`, `reminder_to`, `duration_before`, `reminder_every`, `reminder_for`, `deleted`) VALUES
-(1, 'test reminder', 3, 1, 3, 2, 3, 0);
+(1, 'test reminder', 3, 1, 3, 2, 3, 0),
+(2, 'Weekly reminder', 3, 2, 2, 2, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -230,6 +391,7 @@ INSERT INTO `tbl_doc_escalation_n_reminders` (`id`, `name`, `doc_type`, `reminde
 -- Table structure for table `tbl_escalation_matrix`
 --
 
+DROP TABLE IF EXISTS `tbl_escalation_matrix`;
 CREATE TABLE `tbl_escalation_matrix` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -244,6 +406,7 @@ CREATE TABLE `tbl_escalation_matrix` (
 -- Table structure for table `tbl_knowledge_base`
 --
 
+DROP TABLE IF EXISTS `tbl_knowledge_base`;
 CREATE TABLE `tbl_knowledge_base` (
   `id` int(11) NOT NULL,
   `type_id` int(11) DEFAULT NULL,
@@ -269,6 +432,7 @@ INSERT INTO `tbl_knowledge_base` (`id`, `type_id`, `title`, `solution`, `created
 -- Table structure for table `tbl_knowledge_base_ticket`
 --
 
+DROP TABLE IF EXISTS `tbl_knowledge_base_ticket`;
 CREATE TABLE `tbl_knowledge_base_ticket` (
   `id` int(11) NOT NULL,
   `ticket_id` int(11) DEFAULT NULL,
@@ -292,6 +456,7 @@ INSERT INTO `tbl_knowledge_base_ticket` (`id`, `ticket_id`, `solution_id`, `crea
 -- Table structure for table `tbl_knowledge_base_types`
 --
 
+DROP TABLE IF EXISTS `tbl_knowledge_base_types`;
 CREATE TABLE `tbl_knowledge_base_types` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -314,6 +479,7 @@ INSERT INTO `tbl_knowledge_base_types` (`id`, `name`, `deleted`) VALUES
 -- Table structure for table `tbl_legal_case_procedures`
 --
 
+DROP TABLE IF EXISTS `tbl_legal_case_procedures`;
 CREATE TABLE `tbl_legal_case_procedures` (
   `id` int(11) NOT NULL,
   `legal_case` int(11) NOT NULL,
@@ -336,6 +502,7 @@ INSERT INTO `tbl_legal_case_procedures` (`id`, `legal_case`, `date`, `assigned`,
 -- Table structure for table `tbl_legal_documents`
 --
 
+DROP TABLE IF EXISTS `tbl_legal_documents`;
 CREATE TABLE `tbl_legal_documents` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -358,7 +525,8 @@ CREATE TABLE `tbl_legal_documents` (
 --
 
 INSERT INTO `tbl_legal_documents` (`id`, `name`, `document_type`, `file_document`, `user_responsible`, `status`, `covered_from`, `covered_to`, `deleted`, `reminder`, `reminder_sent`, `completed_escalations`, `created_at`, `updated_at`) VALUES
-(2, 'New s', 2, 'a:0:{}', 89, 1, '2017-12-05', '2017-12-13', 0, 0, 0, 0, '2017-12-05 07:09:31', '0000-00-00 00:00:00');
+(2, 'New s', 2, 'a:0:{}', 89, 1, '2017-12-05', '2017-12-13', 0, 0, 0, 0, '2017-12-05 07:09:31', '0000-00-00 00:00:00'),
+(3, 'terest', 2, 'a:0:{}', 115, 1, '2017-12-15', '2017-12-22', 0, 0, 0, 0, '2017-12-14 12:28:42', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -366,6 +534,7 @@ INSERT INTO `tbl_legal_documents` (`id`, `name`, `document_type`, `file_document
 -- Table structure for table `tbl_legal_document_types`
 --
 
+DROP TABLE IF EXISTS `tbl_legal_document_types`;
 CREATE TABLE `tbl_legal_document_types` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -390,6 +559,7 @@ INSERT INTO `tbl_legal_document_types` (`id`, `name`, `has_expiry`, `can_upload`
 -- Table structure for table `tbl_migartions`
 --
 
+DROP TABLE IF EXISTS `tbl_migartions`;
 CREATE TABLE `tbl_migartions` (
   `version` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
@@ -407,6 +577,7 @@ INSERT INTO `tbl_migartions` (`version`) VALUES
 -- Table structure for table `tbl_time_durations`
 --
 
+DROP TABLE IF EXISTS `tbl_time_durations`;
 CREATE TABLE `tbl_time_durations` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(200) NOT NULL,
@@ -450,6 +621,42 @@ ALTER TABLE `tbl_case_status`
 --
 ALTER TABLE `tbl_case_type`
   ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `tbl_checklists`
+--
+ALTER TABLE `tbl_checklists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_checklist_performed_on`
+--
+ALTER TABLE `tbl_checklist_performed_on`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_checklist_status`
+--
+ALTER TABLE `tbl_checklist_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_checklist_tasks`
+--
+ALTER TABLE `tbl_checklist_tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_checklist_task_checks`
+--
+ALTER TABLE `tbl_checklist_task_checks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_checklist_types`
+--
+ALTER TABLE `tbl_checklist_types`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_cust_supplier_checks`
@@ -568,20 +775,50 @@ ALTER TABLE `tbl_case_status`
 ALTER TABLE `tbl_case_type`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT for table `tbl_checklists`
+--
+ALTER TABLE `tbl_checklists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `tbl_checklist_performed_on`
+--
+ALTER TABLE `tbl_checklist_performed_on`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tbl_checklist_status`
+--
+ALTER TABLE `tbl_checklist_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `tbl_checklist_tasks`
+--
+ALTER TABLE `tbl_checklist_tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `tbl_checklist_task_checks`
+--
+ALTER TABLE `tbl_checklist_task_checks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `tbl_checklist_types`
+--
+ALTER TABLE `tbl_checklist_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `tbl_cust_supplier_checks`
 --
 ALTER TABLE `tbl_cust_supplier_checks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `tbl_cust_supplier_specific_checks`
 --
 ALTER TABLE `tbl_cust_supplier_specific_checks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=411;
 --
 -- AUTO_INCREMENT for table `tbl_cust_supp_checkitems`
 --
 ALTER TABLE `tbl_cust_supp_checkitems`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT for table `tbl_cust_supp_status`
 --
@@ -591,7 +828,7 @@ ALTER TABLE `tbl_cust_supp_status`
 -- AUTO_INCREMENT for table `tbl_doc_escalation_n_reminders`
 --
 ALTER TABLE `tbl_doc_escalation_n_reminders`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tbl_escalation_matrix`
 --
@@ -621,7 +858,7 @@ ALTER TABLE `tbl_legal_case_procedures`
 -- AUTO_INCREMENT for table `tbl_legal_documents`
 --
 ALTER TABLE `tbl_legal_documents`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `tbl_legal_document_types`
 --
