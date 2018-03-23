61deb0c13ab4601b917af26acb219c62e3d2bad9
geowan <you@example.com>
2018-03-23 14:09:16 +0300

    latest reportified

--- vuejsfiles/helpers/appdetails.js
+++ vuejsfiles/helpers/appdetails.js
@@ -1,6 +1,6 @@
 const appdetails = {
-    approoturl:'http://127.0.0.1/pro-kazi/'
-    //approoturl:'http://173.212.247.73:8080/pro.teamkazi.com/'
+    //approoturl:'http://127.0.0.1/pro-kazi/'
+    approoturl:'http://173.212.247.73:8080/pro.teamkazi.com/'
 }
 
 export  default  appdetails
