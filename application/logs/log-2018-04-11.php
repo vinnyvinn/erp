81f6737d2a0da5aadfd75bb421dc8b9a26bfea70
Wagura Maurice <wagura465@gmail.com>
2018-04-11 15:27:42 +0300

    http://esl.dnsalias.com:8080/

---
+++ application/logs/log-2018-04-11.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-04-11 14:25:34 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:25:34 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:25:34 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:25:35 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:25:46 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:25:46 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:25:46 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:25:46 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:26:34 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:26:34 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:26:35 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:26:35 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:26:47 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:26:47 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:26:47 --> Language file contains no data: language/english/custom_lang.php
+ERROR - 2018-04-11 14:26:47 --> Language file contains no data: language/english/custom_lang.php
